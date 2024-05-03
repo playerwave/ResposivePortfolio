32dcea3244313701c17b4b2144c058fb10c5302f
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-05-03 21:37:18 +0700

    Create About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./Component/NavBar/NavBar";
 import Hero from "./Component/Hero/Hero";
+import About from "./Component/About/About";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Hero></Hero>
+      <About></About>
     </>
   );
 }
---
+++ src/Component/About/About.jsx
@@ -0,0 +1,67 @@
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_profile.svg";
+
+function About() {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I'm an experience Frontend Developer with over a decade of
+              professional expertise in the field.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta,
+              consequatur vel ipsum itaque ad dolor commodi, consectetur
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Next JS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECT COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
